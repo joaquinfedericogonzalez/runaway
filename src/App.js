584b51c468a91f145c9367295fa9e5902c873bc8
Joaquin Federico Gonzalez <joaquinfedgonzalez@gmail.com>
2021-10-13 16:59:30 -0300

    arreglo de mayusculas y nuevo favicon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-import { ItemListContainer } from './components/itemListContainer/itemListContainer';
+import { ItemListContainer } from './components/itemListContainer/ItemListContainer';
 import { Nav } from './components/Nav';
 
 function App() {
